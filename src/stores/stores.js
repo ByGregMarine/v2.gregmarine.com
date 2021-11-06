68ea5f35b916956f2d708ba3554548b8f7bf7c12
Greg Marine <gregmarine@hey.com>
2021-11-06 11:41:34 -0400

    Refactored index stores

--- src/stores/stores.js
+++ src/stores/stores.js
@@ -4,54 +4,28 @@ export const pageTitle = writable('Home');
 export const collectionName = writable('');
 export const documentName = writable('');
 
-export const dadjokes = readable([], function (set) {
-  const fetchDadJokeIndex = async () => {
-    const response = await fetch('collections/dadjokes/index.json');
-    const data = await response.json();
-    
-    set(data);
-  }
+const fetchIndex = async (collection) => {
+  const response = await fetch(`collections/${collection}/index.json`);
+  const data = await response.json();
+  return data;
+};
 
-  fetchDadJokeIndex();
-
+export const zen = readable([], function (set) {
+  fetchIndex('zen').then((data) => set(data));
   return () => {};
 });
 
-export const zen = readable([], function (set) {
-  const fetchZenIndex = async () => {
-    const response = await fetch('collections/zen/index.json');
-    const data = await response.json();
-    
-    set(data);
-  }
-
-  fetchZenIndex();
-
+export const dadjokes = readable([], function (set) {
+  fetchIndex('dadjokes').then((data) => set(data));
   return () => {};
 });
 
 export const blog = readable([], function (set) {
-  const fetchBlogIndex = async () => {
-    const response = await fetch('collections/blog/index.json');
-    const data = await response.json();
-    
-    set(data);
-  }
-
-  fetchBlogIndex();
-
+  fetchIndex('blog').then((data) => set(data));
   return () => {};
 });
 
 export const recipes = readable([], function (set) {
-  const fetchRecipesIndex = async () => {
-    const response = await fetch('collections/recipes/index.json');
-    const data = await response.json();
-    
-    set(data);
-  }
-
-  fetchRecipesIndex();
-
+  fetchIndex('recipes').then((data) => set(data));
   return () => {};
 });
