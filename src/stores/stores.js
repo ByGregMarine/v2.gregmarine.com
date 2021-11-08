dbc066a03f7bf33af67929eb9c228b5fa01de067
Greg Marine <gregmarine@hey.com>
2021-11-07 20:08:59 -0500

    Removing the old index stores

--- src/stores/stores.js
+++ src/stores/stores.js
@@ -1,34 +1,8 @@
-import { readable, writable } from 'svelte/store';
+import { writable } from 'svelte/store';
 
 export const scroll = writable({
   previousTop: 0,
   previousPath: '',
   currentTop: 0,
   currentPath: '',
-});
-
-const fetchIndex = async (collection) => {
-  const response = await fetch(`collections/${collection}/index.json`);
-  const data = await response.json();
-  return data;
-};
-
-export const zen = readable([], function (set) {
-  fetchIndex('zen').then((data) => set(data));
-  return () => {};
-});
-
-export const dadjokes = readable([], function (set) {
-  fetchIndex('dad-jokes').then((data) => set(data));
-  return () => {};
-});
-
-export const blog = readable([], function (set) {
-  fetchIndex('blog').then((data) => set(data));
-  return () => {};
-});
-
-export const recipes = readable([], function (set) {
-  fetchIndex('recipes').then((data) => set(data));
-  return () => {};
 });
