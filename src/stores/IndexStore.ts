a308662dd12e59ed1b902340bd6d5e23a423ca8f
Greg Marine <gregmarine@hey.com>
2021-11-20 13:00:03 -0500

    Filtering content on the published field

--- src/stores/IndexStore.ts
+++ src/stores/IndexStore.ts
@@ -6,22 +6,76 @@ const fetchIndex = async (collection) => {
   return data;
 };
 
+export interface Zen {
+  readonly id: string;
+  readonly published: string;
+  readonly title: string;
+  readonly text: string;
+}
 export const zen = readable([], function (set) {
-  fetchIndex('zen').then((data) => set(data));
+  fetchIndex('zen').then((data) => {
+    const today = new Date();
+    set(data.filter((item: Zen) => {
+      const published = new Date(item.published);
+      return published.getTime() <= today.getTime();
+    }));
+  });
   return () => {};
 });
 
+export interface DadJoke {
+  readonly id: string;
+  readonly published: string;
+  readonly title: string;
+  readonly youtube: string;
+  readonly setup: string;
+  readonly punchline: string;
+}
 export const dadJokes = readable([], function (set) {
-  fetchIndex('dad-jokes').then((data) => set(data));
+  fetchIndex('dad-jokes').then((data) => {
+    const today = new Date();
+    set(data.filter((item: DadJoke) => {
+      const published = new Date(item.published);
+      return published.getTime() <= today.getTime();
+    }));
+  });
   return () => {};
 });
 
+export interface Blog {
+  readonly id: string;
+  readonly published: string;
+  readonly title: string;
+  readonly author: string;
+  readonly tags: string[];
+  readonly text: string;
+}
 export const blog = readable([], function (set) {
-  fetchIndex('blog').then((data) => set(data));
+  fetchIndex('blog').then((data) => {
+    const today = new Date();
+    set(data.filter((item: Blog) => {
+      const published = new Date(item.published);
+      return published.getTime() <= today.getTime();
+    }));
+  });
   return () => {};
 });
 
+export interface Recipe {
+  readonly id: string;
+  readonly published: string;
+  readonly title: string;
+  readonly author: string;
+  readonly tags: string[];
+  readonly text: string;
+}
 export const recipes = readable([], function (set) {
-  fetchIndex('recipes').then((data) => set(data));
+  fetchIndex('recipes').then((data) => {
+    const today = new Date();
+    set(data.filter((item: Recipe) => {
+      const published = new Date(item.published);
+      return published.getTime() <= today.getTime();
+    }));
+  });
   return () => {};
 });
